185cbb330ec36aedc194672edb18c2f319b68884
Kenneth <khines214263@gmail.com>
2026-03-15 01:10:16 -0400

    3/15/26

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener("install", function(event) {
+    console.log("Service Worker Installed");
+});
+self.addEventListener("fetch", function(event) {
+    event.respondWith(fetch(event.request));
+});
